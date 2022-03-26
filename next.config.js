fc931e32008c2dcc5e4823115782adbc0075fb15
Heemank Verma <heemankv@gmail.com>
2022-03-26 22:27:07 +0530

    final stable

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-}
+};
+
+module.exports = {
+  images: {
+    domains: ["ipfs.infura.io"],
+  },
+};
