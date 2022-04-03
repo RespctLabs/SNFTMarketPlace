e8ab24e318ba58e739da73e8c9ed9806605e2811
Heemank Verma <heemankv@gmail.com>
2022-04-03 21:03:37 +0530

    host

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-  export const ParentAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const ChildAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+  export const ParentAddress = "0x7cb0742491FE6f6aE1C1464E8Fa9B578aD24cf7e"
+  export const ChildAddress = "0xF79Ef58105e44134f9FC2f309519AE04903860d6"
 
   
