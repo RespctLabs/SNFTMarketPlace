97e20432b2f1a571b5ed8fea06013e75e0b7c719
Heemank Verma <heemankv@gmail.com>
2022-04-03 20:45:37 +0530

    testing

--- pages/Profile.js
+++ pages/Profile.js
@@ -22,197 +22,115 @@ function Profile() {
   const [description, setDescription] = React.useState("2");
   const { getProvider } = useContext(BlockchainContext);
 
-    useEffect(() => {
-        console.log("hello");
-        async function getM() {
-            const provider = await getProvider();
-            const signer = await provider?.getSigner();
-            console.log(signer, provider, " signer  provider");
+  useEffect(() => {
+    console.log("hello");
+    async function getM() {
+      const provider = await getProvider();
+      const signer = await provider?.getSigner();
+      console.log(signer, provider, " signer  provider");
 
-            let parentContract = new ethers.Contract(
-                ParentAddress,
-                ParentContract.abi,
-                signer
-            );
+      let parentContract = new ethers.Contract(
+        ParentAddress,
+        ParentContract.abi,
+        signer
+      );
 
-            try {
-                let count = await parentContract.getComposableCount();
-                console.log("count", count);
-                setNumberOfNFTs(parseInt(BigInt(count._hex).toString(10)));
-                console.log("count", NumberOfNFTs);
-            } catch (err) {
-                console.log("count");
-            }
-
-            // getMintedNFTs();
-        }
-        getM();
-    });
-
-    async function getMintedNFTs() {
-        const provider = await getProvider();
-        const signer = await provider?.getSigner();
-
-        // calling smart contract on function getComposableCount
-        let parentContract = new ethers.Contract(
-            ParentAddress,
-            ParentContract.abi,
-            signer
-        );
-
+      try {
         let count = await parentContract.getComposableCount();
+        console.log("count", count);
+        setNumberOfNFTs(parseInt(BigInt(count._hex).toString(10)));
+        console.log("count", NumberOfNFTs);
+      } catch (err) {
+        console.log("count");
+      }
 
-        console.log(count, "count");
-
-        // // converting bignumber value into string
-        // let tokenId = count.toString();
-        // console.log(tokenId, "tokenId");
-        // setBasic((prevvalue) => ({
-        //   ...prevvalue,
-        //   tokenId: tokenId,
-        // }));
-
-        // console.log(basic, "basic");
-
-        // let url = "";
-
-        // try {
-        //   const added = await client.add(basic.toString(), {
-        //     progress: (prog) => console.log(`received: ${prog}`),
-        //   });
-        //   url = `https://ipfs.infura.io/ipfs/${added.path}`;
-        //   console.log(url, "url");
-        //   setFileUrl(url);
-        // } catch (error) {
-        //   console.log("Error uploading file: ", error);
-        // }
-
-        // // let x = { tokenId: url };
-        // console.log(tokenId, url);
-        // // await writeJsonFile(data, x);
+      // getMintedNFTs();
     }
+    getM();
+  });
 
-    function generateNFTs(event) {
-        let Nfts = [];
-        for (var i = 1; i <= NumberOfNFTs; i++) {
-            Nfts.push(
-                <Link href={"/nfts/" + i} passHref>
-                    <a>
-                        <NFTCard key={i} number={i} element={""} />
-                    </a>
-                </Link>
-            );
-        }
-        Nfts.push(
-            <Link href={"/nfts/" + (NumberOfNFTs + 1)} passHref>
-                <a>
-                    <div className=" headerDiv p-2 m-8 rounded-lg bg-OurBlack w-60 flex flex-col justify-center">
-                        <div className="flex justify-between items-center mx-auto text-white hover:text-black font-bold text-2xl text-center w-52 h-52 bg-[#464646] rounded-lg">
-                            MINT your own NFT
-                        </div>
-                        <div className="flex justify-between text-white mx-3">
-                            <p className="font-semibold">Respct NFT</p>
-                            <p className="text-OurGreen">Level sNFT</p>
-                        </div>
-                        <div className="flex justify-end text-white mx-3">
-                            {/* <p className="text-OurPurple">Top Bid 0</p> */}
-                        </div>
-                    </div>
-                </a>
-            </Link>
-        );
-        return (
-            <div id="cards" className="md:grid md:grid-cols-4 md:gap-12">
-                {Nfts}
-            </div>
-        );
-    }
+  async function getMintedNFTs() {
+    const provider = await getProvider();
+    const signer = await provider?.getSigner();
 
-    return (
-        <div className=" container mx-auto mb-16">
-            <div className="mx-4 md:mx-0">
-                <Image src={Respct} />
-            </div>
-            <div className="flex justify-center ">
-                <div className="md:absolute md:top-80">
-                    <Image src={ProfileImage} layout="fixed" alt="profile" />
-                </div>
-            </div>
-
-            <div className="flex flex-col md:flex-row md:pt-12 ">
-                <div id="profileLeft" className="md:w-3/5 md:pr-12">
-                    {/* <div className="flex justify-start flex-col">
-            <div>{props.name ? props.name.toUpperCase() : "ASUKI"}</div>
-            <div>
-              {props.collection
-                ? props.collection.toUpperCase()
-                : "1000 NFT collections"}
-            </div>
-          </div> */}
-                    <p className="text-sm mx-4 md:mx-0 md:text-base text-center md:text-left md:w-4/5">
-                        {!description // note the !
-                            ? description.toUpperCase()
-                            : " A brand for the metaverse. Built by the community. View the collection at azuki.com/gallery. Azuki starts with a collection of10,000 avatars that give you membership access to The Garden: a corner of the internet where artists, builders, and web3 enthusiasts meet to create a decentralized future. Azuki holders receive access to exclusive drops, experiences, and more. Visit azuki.com for more details."}
-                    </p>
-                </div>
-                <div
-                    id="profileRight"
-                    className="my-3 md:my-0 mx-4 md:mx-0 md:w-2/5 md:pt-9 md:px-3"
-                >
-                    {/* <TwitterShareButton
-                        title={"gathering enagement points to level up my nft "}
-                        url={"@RespctClub"}
-                        onShareWindowClose={(e) =>
-                            console.log("onShareWindowClose")
-                        }
-                    >
-                        Tweet
-                    </TwitterShareButton> */}
-                    {/* <PrimaryButton /> */}
-
-                    <Tity
-                    />
-                </div>
-            </div>
-            {/* <div className="flex justify-between mx-4 md:mx-0 md:pt-6">
-        <div className="flex space-x-5 md:space-x-0">
-          <div>
-            <button className="bg-OurBlack rounded-lg text-[#03AFD0] md:mr-6 md:ml-2">
-              Collection
-            </button>
-          </div>
-          <div>
-            <button className="bg-OurBlack rounded-lg text-[#03AFD0] md:mx-6">
-              Owned
-            </button>
-          </div>
-          <div>
-            <button className="bg-OurBlack rounded-lg text-[#03AFD0] md:mx-6">
-              Profile
-            </button>
-          </div>
-          <div>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                getMintedNFTs();
-              }}
-              className="bg-OurBlack rounded-lg text-[#03AFD0] md:mx-6"
-            >
-              getComposableCount
-            </button>
-          </div>
-        </div>
-        <div>
-          <button className="bg-white text-black rounded-2xl drop-shadow-white px-2 md:px-5 py-1 md:mr-10">
-            Filter &nbsp; &nbsp;
-            <Image src={Filter} layout="fixed" />
-          </button>
-        </div>
-      </div> */}
-            <div>{generateNFTs()}</div>
-        </div>
+    // calling smart contract on function getComposableCount
+    let parentContract = new ethers.Contract(
+      ParentAddress,
+      ParentContract.abi,
+      signer
     );
+
+    let count = await parentContract.getComposableCount();
+
+    console.log(count, "count");
+  }
+
+  function generateNFTs(event) {
+    let Nfts = [];
+    for (var i = 1; i <= NumberOfNFTs; i++) {
+      Nfts.push(
+        <Link href={"/nfts/" + i} passHref>
+          <a>
+            <NFTCard key={i} number={i} element={""} />
+          </a>
+        </Link>
+      );
+    }
+    Nfts.push(
+      <Link href={"/nfts/" + (NumberOfNFTs + 1)} passHref>
+        <a>
+          <div className=" headerDiv p-2 m-8 rounded-lg bg-OurBlack w-60 flex flex-col justify-center">
+            <div className="flex justify-between items-center mx-auto text-white hover:text-black font-bold text-2xl text-center w-52 h-52 bg-[#464646] rounded-lg">
+              MINT your own NFT
+            </div>
+            <div className="flex justify-between text-white mx-3">
+              <p className="font-semibold">Respct NFT</p>
+              <p className="text-OurGreen">Level sNFT</p>
+            </div>
+            <div className="flex justify-end text-white mx-3">
+              {/* <p className="text-OurPurple">Top Bid 0</p> */}
+            </div>
+          </div>
+        </a>
+      </Link>
+    );
+    return (
+      <div id="cards" className="md:grid md:grid-cols-4 md:gap-12">
+        {Nfts}
+      </div>
+    );
+  }
+
+  return (
+    <div className=" container mx-auto mb-16">
+      <div className="mx-4 md:mx-0">
+        <Image src={Respct} />
+      </div>
+      <div className="flex justify-center ">
+        <div className="md:absolute md:top-80">
+          <Image src={ProfileImage} layout="fixed" alt="profile" />
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row md:pt-12 ">
+        <div id="profileLeft" className="md:w-3/5 md:pr-12">
+          <p className="text-sm mx-4 md:mx-0 md:text-base text-center md:text-left md:w-4/5">
+            {!description // note the !
+              ? description.toUpperCase()
+              : " A brand for the metaverse. Built by the community. View the collection at azuki.com/gallery. Azuki starts with a collection of10,000 avatars that give you membership access to The Garden: a corner of the internet where artists, builders, and web3 enthusiasts meet to create a decentralized future. Azuki holders receive access to exclusive drops, experiences, and more. Visit azuki.com for more details."}
+          </p>
+        </div>
+        <div
+          id="profileRight"
+          className="my-3 md:my-0 mx-4 md:mx-0 md:w-2/5 md:pt-9 md:px-3"
+        >
+          <Tity />
+        </div>
+      </div>
+
+      <div>{generateNFTs()}</div>
+    </div>
+  );
 }
 
 Profile.layout = "L1";
