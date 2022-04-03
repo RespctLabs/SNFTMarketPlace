e64856a24a72b7d4fd43212223c7a35a3dd79e55
sc0rp10n-lappy-wslubuntu <pragyan52yadav@gmail.com>
2022-04-03 19:33:10 +0530

    nfts items ui update

--- components/common/PrimaryButton.tsx
+++ components/common/PrimaryButton.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 export default function PrimaryButton(props) {
-  const color = "bg-" + props.color;
-  const shadow = "shadow-" + props.shadow;
+    const color = "bg-" + props.color;
+    const shadow = "shadow-" + props.shadow;
 
-  return (
-    <button
-      onClick={props.onClick}
-      className={color + " rounded-md shadow-md " + shadow}
-    >
-      <div className="text-white font-bold px-12 py-1">
-        {props.text ? props.text : "Loading Text..."}
-      </div>
-    </button>
-  );
+    return (
+        <div className="flex justify-center">
+            <button
+                onClick={props.onClick}
+                className={color + " rounded-md shadow-md " + shadow}
+            >
+                <div className="text-white font-bold px-12 py-1">
+                    {props.text ? props.text : "Loading Text..."}
+                </div>
+            </button>
+        </div>
+    );
 }
