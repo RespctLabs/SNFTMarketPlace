19680dc1140451195ed1f8a7934d2b95aa163dc5
sc0rp10n-lappy-wslubuntu <pragyan52yadav@gmail.com>
2022-04-08 10:34:26 +0530

    primary button fix

--- components/common/PrimaryButton.tsx
+++ components/common/PrimaryButton.tsx
@@ -1,19 +1,44 @@
 import React from "react";
 
-export default function PrimaryButton(props) {
-    const color = "bg-" + props.color;
-    const shadow = "shadow-" + props.shadow;
-
-    return (
-        <div className="flex justify-center">
-            <button
-                onClick={props.onClick}
-                className={color + " rounded-md shadow-md " + shadow}
-            >
-                <div className="text-white font-bold px-12 py-1">
-                    {props.text ? props.text : "Loading Text..."}
-                </div>
-            </button>
-        </div>
-    );
+export default function PrimaryButton({ flag, onCli }) {
+    if (flag === "checkEngagement") {
+        return (
+            <div className="flex justify-center">
+                <button
+                    onClick={onCli}
+                    className="bg-[#7834BF] rounded-md shadow-md"
+                >
+                    <div className="text-white font-bold px-12 py-1">
+                        Check Engagement
+                    </div>
+                </button>
+            </div>
+        );
+    } else if (flag === "buy") {
+        return (
+            <div className="flex justify-center">
+                <button
+                    onClick={onCli}
+                    className="bg-OurBlue rounded-md shadow-md"
+                >
+                    <div className="text-white font-bold px-12 py-1">
+                        Buy
+                    </div>
+                </button>
+            </div>
+        );
+    } else if (flag === "upgrade") {
+        return (
+            <div className="flex justify-center">
+                <button
+                    onClick={onCli}
+                    className="bg-[#7834BF] rounded-md shadow-md"
+                >
+                    <div className="text-white font-bold px-12 py-1">
+                        Upgrade
+                    </div>
+                </button>
+            </div>
+        );
+    }
 }
